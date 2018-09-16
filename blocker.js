1dd96195e11dbb71f38f7a6c28bba232200c5e2f
Emma Lubin <elubin@godaddy.com>
2018-09-16 09:12:33 -0700

    Fix unit test

--- blocker.js
+++ blocker.js
@@ -1,18 +1,21 @@
 function getMeetingsByDate(calendarResults) {
   const eventsByDate = {};
-  for (event in calendarResults) {
+  calendarResults.value.forEach(event => {
     const startDate = event.start.dateTime;
     const endDate = event.end.dateTime;
     // Assume events don't span multiple days.
     const date = startDate.split('T')[0];
-    const startTime = startDate.getTime();
-    const endTime = endDate.getTime();
-    if (eventsByDate.date) {
-      eventsByDate.date.push(endTime - startTime);
+    const meetingBounds = {
+      start: startDate,
+      end: endDate
+    }
+    if (eventsByDate[date]) {
+      eventsByDate[date].push(meetingBounds);
     } else {
-      eventsByDate.date = [endTime - startTime];
+      eventsByDate[date] = [meetingBounds];
     }
   });
+    return eventsByDate;
 }
 
 module.exports = {
--- test/blocker.test.js
+++ test/blocker.test.js
@@ -1,12 +1,22 @@
 const { expect } = require('chai');
-const calendarResponse = require('calendar-response.json');
+
+const calendarResponse = require('./mocks/calendar-response.json');
 const { getMeetingsByDate } = require('../blocker');
 
-describe('blocker', () = {
-    describe('.getMeetingsByDate', () => {
-	it('bins meetings by date', () => {
-	    expect(getMeetingsByDate(calendarResponse)).to.equal('foo');
-	});
+describe('blocker', () => {
+  describe('.getMeetingsByDate', () => {
+    it('bins meetings by date', () => {
+      const expected = {
+        '2017-11-16': [{
+          'start': '2017-11-16T08:00:00.0000000',
+          'end': '2017-11-16T08:30:00.0000000'
+        }],
+        '2017-11-17': [{
+          'start': '2017-11-17T10:00:00.0000000',
+          'end': '2017-11-17T11:30:00.0000000'
+        }],
+      }
+      expect(getMeetingsByDate(calendarResponse)).to.deep.equal(expected);
+    });
   });
-
 });
