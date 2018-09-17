9fcf92859aa8cad07343c998e1b39f3c5242871d
Silas Boyd-Wickizer <silasbw@gmail.com>
2018-09-17 07:08:43 -0700

    feat(authorization): tweak authorization handling (#23)
    Now we store authorization in the "db" and use the ID Token to authenticate a
    user.

--- routes/authorize.js
+++ routes/authorize.js
@@ -6,29 +6,23 @@ const db = require('../lib/db')
 router.get('/', async function (req, res, next) {
   const code = req.query.code
   if (code) {
-    let authToken
-
     try {
-      authToken = await authHelper.getTokenFromCode(code)
-      authHelper.saveValuesToCookie(authToken, res)
+      const { authToken, idToken } = await authHelper.getTokenFromCode(code)
+
+      res.cookie('srt_id_token', idToken, { maxAge: 7200000, httpOnly: true })
       db.setToken(authToken.user.name, authToken)
     } catch (error) {
       res.render('error', { title: 'Error', message: 'Error exchanging code for token', error: error })
     }
 
-    // Redirect to home
     res.redirect('/')
   } else {
-    // Otherwise complain
     res.render('error', { title: 'Error', message: 'Authorization error', error: { status: 'Missing code parameter' } })
   }
 })
 
-/* GET /authorize/signout */
 router.get('/signout', function (req, res, next) {
-  authHelper.clearCookies(res)
-
-  // Redirect to home
+  res.clearCookie('srt_id_token', { maxAge: 7200000, httpOnly: true })
   res.redirect('/')
 })
 
